3950527ea10dcf31b0930e498f8afe61fb7e58ce
Chris Hunt <chrishunt@github.com>
2013-08-11 11:22:44 -0700

    Fix nitpicks for http://goo.gl/3IbXkd

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,29 +1,34 @@
 class Bob
   def hey(greeting)
-    adult = ResponsibleAdult.new(greeting)
+    respond_to ResponsibleAdult.new(greeting)
+  end
 
-    if adult.is_silent?
-      'Fine. Be that way!'
-    elsif adult.is_yelling?
-      'Woah, chill out!'
-    elsif adult.is_asking_question?
-      'Sure.'
-    else
-      'Whatever.'
+  private
+
+  def respond_to(adult)
+    case
+      when adult.is_silent?
+        'Fine. Be that way!'
+      when adult.is_yelling?
+        'Woah, chill out!'
+      when adult.is_asking_question?
+        'Sure.'
+      else
+        'Whatever.'
     end
   end
 
   ResponsibleAdult = Struct.new(:greeting) do
     def is_silent?
-      greeting.to_s =~ /^\s*$/
+      greeting.to_s.strip.empty?
     end
 
     def is_yelling?
-      greeting =~ /^[^a-z]+$/
+      greeting == greeting.upcase
     end
 
     def is_asking_question?
-      greeting =~ /\?$/
+      greeting.end_with? '?'
     end
   end
 end
