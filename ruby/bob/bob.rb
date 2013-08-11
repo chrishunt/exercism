7825584e3c10f2208cecc023c68d9293cc6a01a6
Chris Hunt <chrishunt@github.com>
2013-08-11 10:22:16 -0700

    Fix nitpics for http://goo.gl/jmDO3K

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,28 +1,28 @@
 class Bob
   def hey(greeting)
-    you = You.new greeting
+    adult = ResponsibleAdult.new(greeting)
 
-    if you.are_silent?
+    if adult.is_silent?
       'Fine. Be that way!'
-    elsif you.yell_at_me?
+    elsif adult.is_yelling?
       'Woah, chill out!'
-    elsif you.ask_me_a_question?
+    elsif adult.is_asking_question?
       'Sure.'
     else
       'Whatever.'
     end
   end
 
-  You = Struct.new(:greeting) do
-    def are_silent?
+  ResponsibleAdult = Struct.new(:greeting) do
+    def is_silent?
       greeting.to_s =~ /^\s*$/
     end
 
-    def yell_at_me?
+    def is_yelling?
       greeting =~ /^[^a-z]+$/
     end
 
-    def ask_me_a_question?
+    def is_asking_question?
       greeting =~ /\?$/
     end
   end
